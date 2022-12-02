bf3faad1a26ff6625787bd1a6cf14c1236a3a6b9
my-yilmaz <myyilmaz046@gmail.com>
2022-12-02 17:35:41 +0300

    v5

---
+++ src/main/java/lessons/lesson07_stringmanipulations/C05_StringManipulations.java
@@ -0,0 +1,10 @@
+package lessons.lesson07_stringmanipulations;
+
+import java.util.Scanner;
+
+public class C05_StringManipulations {
+    public static void main(String[] args) {
+        String a= "Java kolaydir.";
+
+    }
+}
