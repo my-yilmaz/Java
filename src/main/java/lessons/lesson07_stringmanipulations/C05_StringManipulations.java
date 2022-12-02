217dde359c832ea589b245f83fc146a57d2ebf86
my-yilmaz <myyilmaz046@gmail.com>
2022-12-02 17:41:30 +0300

    v5

--- src/main/java/lessons/lesson07_stringmanipulations/C05_StringManipulations.java
+++ src/main/java/lessons/lesson07_stringmanipulations/C05_StringManipulations.java
@@ -6,5 +6,13 @@ public class C05_StringManipulations {
     public static void main(String[] args) {
         String a= "Java kolaydir.";
 
+        // startsWith("kola",5) kodu ilk 5 karakterden sonraki String'e bakar ve o String'in "kola" ile baslayip baslamadigini kontrol eder.
+        // "kola" ile basliyorsa "true", baslamiyorsa "false" return eder.
+        boolean b=a.startsWith("va",2);
+
+        System.out.println(b); //true==>  bu stringin 2. karakterden sonra
+        //         ne ile basladigini bulur ve karsilastirir
+
+
     }
 }
