72e6312e7ac966c0b4efabe9a41dca7e7cddcc5f
my-yilmaz <myyilmaz046@gmail.com>
2023-01-19 21:28:49 +0300

    v28

---
+++ src/main/java/lessons/lesson23_maps/C02_HashMap.java
@@ -0,0 +1,17 @@
+package lessons.lesson23_maps;
+
+import java.util.HashMap;
+
+public class C02_HashMap {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> studentAges = new HashMap<>();
+
+        studentAges.put("Ali", 13);
+        studentAges.put("Tom", 21);
+        studentAges.put("Brad", 12);
+        studentAges.put("Ajda", 76);
+        studentAges.put("Cuneyt", 75);
+
+    }
+}
