60177be91a0b7e1d62956883f9a384058ec0e8e2
my-yilmaz <myyilmaz046@gmail.com>
2023-01-31 20:17:27 +0300

    v29

--- src/main/java/lessons/lesson23_maps/C05_StaticBlock.java
+++ src/main/java/lessons/lesson23_maps/C05_StaticBlock.java
@@ -2,4 +2,24 @@ package lessons.lesson23_maps;
 
 public class C05_StaticBlock {
 
+    static {
+        System.out.println("Static block 2");
+    }
+
+    public static double pi;
+
+    //* static block'lar ihtiyacimiz olan variable'larin class olusturma safhasinda elimizde olmasini saglar.
+    //* static block'lar class icindeki herseyden once calistirilir. "main method" dan ve diger tum method'lardan once calistirilir.
+    //* static block'lar icinde sadece static variable'lara deger atanabilir.
+    //* 1'den fazla static block varsa ustteki once calistirilir.
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+
+    static {
+        pi = 3.14;
+        System.out.println("Static block 1");
+    }
 }
+
--- src/main/java/lessons/lesson23_maps/C06_StaticBlock.java
+++
@@ -1,5 +0,0 @@
-package lessons.lesson23_maps;
-
-public class C06_StaticBlock {
-
-}
