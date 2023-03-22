374ce44c43d5f0b238d08c57970521195a6d97e6
myyilmaz046@gmail.com <myyilmaz046@gmail.com>
2023-03-22 17:11:52 +0300

    v29

---
+++ src/main/java/basicpractice/practice07/C09_StringManipulations.java
@@ -0,0 +1,9 @@
+package basicpractice.practice07;
+
+import java.util.Scanner;
+
+public class C09_StringManipulations {
+    public static void main(String[] args) {
+        System.out.println("merhaba");
+    }
+}
