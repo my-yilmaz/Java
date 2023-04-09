29764ffa9f581591aeec1d9f58535c645c0c6684
myyilmaz046@gmail.com <myyilmaz046@gmail.com>
2023-04-09 23:21:36 +0300

    Q8

--- src/main/java/javaQuestions/interviewQuestions3/Q03_WhileCountSum.java
+++ src/main/java/javaQuestions/interviewQuestions3/Q03_WhileCountSum.java
@@ -1,15 +1,27 @@
 package javaQuestions.interviewQuestions3;
 
+import java.util.Scanner;
+
 public class Q03_WhileCountSum {
-    /*
-    Kullanicidan toplanmak uzere sayilar isteyin sayi adedi 10'u gecerse veya toplam 500 'u gecerse
-    bu kadar sayi yeter  ".. adet sayi girdin, toplami..." yazdirin
+    /**
+     TODO: Kullanicidan toplanmak uzere sayilar isteyin sayi adedi 10'u gecerse veya toplam 500 'u gecerse
+           bu kadar sayi yeter  ".. adet sayi girdin, toplami..." yazdirin
      */
 
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int toplam = 0;
+        int sayac = 0;
 
+        while (sayac < 10 && toplam < 500) {
+            System.out.print("Bir sayi girin: ");
+            int sayi = scanner.nextInt();
+            toplam += sayi;
+            sayac++;
+        }
 
+        System.out.println(sayac + " adet sayi girdiniz, toplamlari " + toplam);
     }
-
 }
+
+
