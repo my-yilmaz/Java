de4844611cfcc669afa96bef76b5d1094aeb36df
myyilmaz046@gmail.com <myyilmaz046@gmail.com>
2023-04-09 23:18:20 +0300

    Q8

--- src/main/java/javaQuestions/interviewQuestions3/Q02_Palindrome.java
+++ src/main/java/javaQuestions/interviewQuestions3/Q02_Palindrome.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Q02_Palindrome {
 
-    /*
+    /**
       Crteate a program checks if a String is PALINDROME or not.
       If a word, phrase, or sequence that reads the same backword as forward then it is called "palindrome",
       For Example : "madam" or "nursesrun" .
---
+++ src/main/java/javaQuestions/interviewQuestions3/Q03_WhileCountSum.java
@@ -0,0 +1,15 @@
+package javaQuestions.interviewQuestions3;
+
+public class Q03_WhileCountSum {
+    /*
+    Kullanicidan toplanmak uzere sayilar isteyin sayi adedi 10'u gecerse veya toplam 500 'u gecerse
+    bu kadar sayi yeter  ".. adet sayi girdin, toplami..." yazdirin
+     */
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
