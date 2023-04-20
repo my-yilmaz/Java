af51747096ceaac6d5d164e5c70c471c37e3d1ac
my-yilmaz <myyilmaz046@gmail.com>
2023-04-21 00:05:09 +0300

    Q17

--- src/main/java/javaQuestions/interviewQuestions6/Q05_ExponentialNumber.java
+++ src/main/java/javaQuestions/interviewQuestions6/Q05_ExponentialNumber.java
@@ -1,17 +1,27 @@
 package javaQuestions.interviewQuestions6;
 
+import java.util.Scanner;
+
 public class Q05_ExponentialNumber {
-    //Get 2 numbers from the user.
-    //first number base
-    //second number Top
-    //Write a code that calculates the prime of a number.
-    // 2, 3 --> 2^3 = 2*2*2= 8
-
-    // 4, 3 --> 4^3= 4*4*4 = 64
-
-
+    /*
+   TODO? Get 2 numbers from the user.
+         first number base
+         second number Top
+         Write a code that calculates the prime of a number.
+         2, 3 --> 2^3 = 2*2*2= 8
+         4, 3 --> 4^3= 4*4*4 = 64
+     */
     public static void main(String[] args) {
-      }
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the base number: ");
+        int base = scanner.nextInt();
+
+        System.out.print("Enter the top number: ");
+        int top = scanner.nextInt();
+
+        int result = (int) Math.pow(base, top);
+        System.out.println(base + "^" + top + " = " + result);
+    }
 
 }
--- src/main/java/javaQuestions/interviewQuestions6/Q06_DuplicateValue.java
+++ src/main/java/javaQuestions/interviewQuestions6/Q06_DuplicateValue.java
@@ -1,9 +1,12 @@
 package javaQuestions.interviewQuestions6;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q06_DuplicateValue {
        /*
-        Write a code that returns the duplicate chars in a String array.
+   TODO: Write a code that returns the duplicate chars in a String array.
         (interview Question)
         Input :
         String str=“Javaisalsoeasy”
@@ -11,9 +14,24 @@ public class Q06_DuplicateValue {
          */
 
     public static void main(String[] args) {
+        String str = "Javaisalsoeasy";
+        char[] charArray = str.toCharArray(); // String'i liste dönüştüruyoruz
 
+        ArrayList<Character> duplicates = new ArrayList<>(); // Tekrar eden karakterleri saklamak için bir ArrayList oluşturuyoruz
+
+        // Her bir karakteri kontrol etmek için bir döngü kullaniyoruz
+        for (int i = 0; i < charArray.length; i++) {
+            int count = 1; // Harf sayacını 1 olarak ayarliyoruz
+            for (int j = i + 1; j < charArray.length; j++) {
+                if (charArray[i] == charArray[j]) {
+                    count++; // Harf sayacını arttır
+                }
+            }
+            if (count > 1 && !duplicates.contains(charArray[i])) {
+                duplicates.add(charArray[i]); // Tekrar eden karakterleri ArrayList'e ekle
+            }
+        }
+
+        System.out.println("Duplicate characters in the String: " + duplicates);
     }
-    
-
 }
-
