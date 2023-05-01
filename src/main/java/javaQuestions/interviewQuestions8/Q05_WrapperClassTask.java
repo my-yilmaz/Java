b634e106c85f5dcb707952ba8c6fa5a614fd4b54
my-yilmaz <myyilmaz046@gmail.com>
2023-05-01 23:00:01 +0300

    Q22

--- src/main/java/javaQuestions/interviewQuestions8/Q05_WrapperClassTask.java
+++ src/main/java/javaQuestions/interviewQuestions8/Q05_WrapperClassTask.java
@@ -1,16 +1,30 @@
 package javaQuestions.interviewQuestions8;
 
 public class Q05_WrapperClassTask {
+/*
+ TODO:  create a method that accepts an int
+        and returns twice of that int
+        overload this method with wrapper class: Integer
+        /
+        int kabul eden bir metot yarat
+        ve bu int'nin iki katını döndürür
+        bu yöntemi wrapper sınıfıyla overload et : Integer
 
-    // create a method that accepts an int
-    // and returns twice of that int
-    // overload this method with wrapper class: Integer
-
-    // int kabul eden bir metot yarat
-    // ve bu int'nin iki katını döndürür
-    // bu yöntemi wrapper sınıfıyla overload et : Integer
+ */
 
     public static void main(String[] args) {
+        int x = 5;
+        System.out.println("2 katı: " + doubleInt(x));
 
+        Integer y = 10;
+        System.out.println("2 katı: " + doubleInt(y));
+    }
+
+    public static int doubleInt(int num) {
+        return num * 2;
+    }
+
+    public static Integer doubleInt(Integer num) {
+        return num * 2;
     }
 }
