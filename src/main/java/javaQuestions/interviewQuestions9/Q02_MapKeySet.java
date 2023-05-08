058320d3ab6c9a33e9399905eb1b8915cb77cc22
my-yilmaz <myyilmaz046@gmail.com>
2023-05-08 22:45:04 +0300

    Q26

--- src/main/java/javaQuestions/interviewQuestions9/Q02_MapKeySet.java
+++ src/main/java/javaQuestions/interviewQuestions9/Q02_MapKeySet.java
@@ -1,13 +1,30 @@
 package javaQuestions.interviewQuestions9;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q02_MapKeySet {
-    // create a map of zipcodes and Cities, at least 3 entries
-    // get the keySet and iterate over it, print the map like below:
-    // zipcode1 => city1
-    // zipcode2 => city2
-    // part 2: print all the keys of the map
+   /*
+ TODO:
+    create a map of zipcodes and Cities, at least 3 entries
+    get the keySet and iterate over it, print the map like below:
+    zipcode1 => city1
+    zipcode2 => city2
+    part 2: print all the keys of the map
+
+    */
 
     public static void main(String[] args) {
+        Map<Integer, String> zipCodeMap = new HashMap<>();
+        zipCodeMap.put(34100, "Istanbul");
+        zipCodeMap.put(30672, "Ankara");
+        zipCodeMap.put(35210, "Izmir");
 
+        for (Integer zipCode : zipCodeMap.keySet()) {
+            System.out.println(zipCode + " => " + zipCodeMap.get(zipCode));
+        }
+        for (Integer zipCode : zipCodeMap.keySet()) {
+            System.out.println(zipCode);
+        }
     }
 }
--- src/main/java/javaQuestions/interviewQuestions9/Q03_PrintMap.java
+++ src/main/java/javaQuestions/interviewQuestions9/Q03_PrintMap.java
@@ -1,15 +1,47 @@
 package javaQuestions.interviewQuestions9;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Q03_PrintMap {
-    //aşağıdaki çıktıyı elde ediniz
-    // Kiraz 100
-    // İncir 200
-    // Enginar 150
-    // Üzüm 145
-    // Nar 250
+   /*
+ TODO:
+      aşağıdaki çıktıyı elde ediniz
+      Kiraz 100
+      İncir 200
+      Enginar 150
+      Üzüm 145
+      Nar 250
+
+    */
 
     public static void main(String[] args) {
+        //Girilen siraya gore almak istersek once blr list olustururuz.
+        List<String> meyveler = Arrays.asList("Kiraz", "İncir", "Enginar", "Üzüm", "Nar");
+        Map<String, Integer> meyveMiktar = new HashMap<>();
+        meyveMiktar.put("Kiraz", 100);
+        meyveMiktar.put("İncir", 200);
+        meyveMiktar.put("Enginar", 150);
+        meyveMiktar.put("Üzüm", 145);
+        meyveMiktar.put("Nar", 250);
 
+        for (String meyve : meyveler) {
+            System.out.println(meyve + " " + meyveMiktar.get(meyve));
+        }
+        /*
+        Map<String, Integer> meyveNufus = new HashMap<>();
+        meyveNufus.put("Kiraz", 100);
+        meyveNufus.put("İncir", 200);
+        meyveNufus.put("Enginar", 150);
+        meyveNufus.put("Üzüm", 145);
+        meyveNufus.put("Nar", 250);
 
+        // Verileri yazdırma
+        for (Map.Entry<String, Integer> entry : meyveNufus.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+         */
     }
 }
