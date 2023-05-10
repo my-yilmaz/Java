7987bfbdc5e84ca03825dbd2137860d40549aaa2
my-yilmaz <myyilmaz046@gmail.com>
2023-05-10 22:55:14 +0300

    Q27

--- src/main/java/javaQuestions/interviewQuestions9/Q06_TreeMapFrekans.java
+++ src/main/java/javaQuestions/interviewQuestions9/Q06_TreeMapFrekans.java
@@ -1,10 +1,27 @@
 package javaQuestions.interviewQuestions9;
 
+import java.util.TreeMap;
+
 public class Q06_TreeMapFrekans {
 
     // TreeMap kullanarak bir cümlenin içindeki harflerin frekansını method ile bulunuz.
 
     public static void main(String[] args) {
+        System.out.println("countLetters(\"Java is easy\") = " + countLetters("Java is easy"));
+    }
 
+    public static TreeMap<Character, Integer> countLetters(String sentence) {
+        // Harf sayacını oluştur
+        TreeMap<Character, Integer> letterCount = new TreeMap<>();
+
+        // Harfleri say
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (Character.isLetter(c)) {
+                letterCount.put(c, letterCount.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        return letterCount;
     }
 }
