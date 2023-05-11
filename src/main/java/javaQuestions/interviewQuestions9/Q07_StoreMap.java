3cec6948cf341663518f6d0f50b3020ec2b5e903
my-yilmaz <myyilmaz046@gmail.com>
2023-05-11 21:49:57 +0300

    Q28

--- src/main/java/javaQuestions/interviewQuestions9/Q07_StoreMap.java
+++ src/main/java/javaQuestions/interviewQuestions9/Q07_StoreMap.java
@@ -1,7 +1,11 @@
 package javaQuestions.interviewQuestions9;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Q07_StoreMap {
-    /* 1- Kullanıcıdan bir kişinin kimlik numarsını(4 haneli), tam ismini, adresini , telefonunu alın.
+    /*
+TODO:  1- Kullanıcıdan bir kişinin kimlik numarsını(4 haneli), tam ismini, adresini , telefonunu alın.
        2- Kimlik numarasını key olarak, diğer bilgileri value olarak bir mape depolayın
        3- Kimlik numarasını girerek kullanıcını bilgilerini ekrana yazdırın
        4- Tüm kullanicilarin isimlerini ekrana yazdıralım
@@ -9,6 +13,52 @@ public class Q07_StoreMap {
 
     public static void main(String[] args) {
 
-    }
+        Scanner scanner = new Scanner(System.in);
 
+        // Bos bir HashMap olusturuyoruz
+        HashMap<String, String[]> kisiler = new HashMap<>();
+        kisiler.put("4455", new String[]{"Bradd Bitt", "Kansas Ctiy", "2715656"});
+        kisiler.put("4456", new String[]{"Angelina Julia", "New York", "2712430"});
+        kisiler.put("4457", new String[]{"Tom Hanks", "Arizona", "2712800"});
+
+        // Kullanicidan bilgileri alip ve HashMap'e kaydedilim
+        System.out.print("Kimlik numarası: ");
+        String kimlikNo = scanner.nextLine();
+
+        System.out.print("Tam adı: ");
+        String tamAd = scanner.nextLine();
+
+        System.out.print("Adresi: ");
+        String adres = scanner.nextLine();
+
+        System.out.print("Telefonu: ");
+        String telefon = scanner.nextLine();
+
+        kisiler.put(kimlikNo, new String[]{tamAd, adres, telefon});
+        System.out.println("***********************");
+        // Kullanicinin bilgilerini ekrana yazdiriralim
+        System.out.print("Bilgilerini görmek istediğiniz kişinin kimlik numarasını girin: ");
+        kimlikNo = scanner.nextLine();
+
+        if (kisiler.containsKey(kimlikNo)) {
+            String[] bilgiler = kisiler.get(kimlikNo);
+
+            System.out.println("Tam adı: " + bilgiler[0]);
+            System.out.println("Adresi: " + bilgiler[1]);
+            System.out.println("Telefonu: " + bilgiler[2]);
+        } else {
+            System.out.println("Bu kimlik numarasına sahip bir kişi bulunamadı.");
+        }
+        System.out.println("***********************");
+        // Tüm kisilerin adlarini ekrana yazdiriralim
+        System.out.println("Tüm kişilerin adları:");
+        for (String[] bilgiler : kisiler.values()) {
+            System.out.println(bilgiler[0]);
+        }
+    }
 }
+
+
+
+
+
--- src/main/java/javaQuestions/interviewQuestions9/Q08_ReadFileTxt.java
+++ src/main/java/javaQuestions/interviewQuestions9/Q08_ReadFileTxt.java
@@ -1,20 +1,59 @@
 package javaQuestions.interviewQuestions9;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Q08_ReadFileTxt {
        /*
-    1)Create a text file inside this package
-    2)Type the following text inside the text file
-       Java is easy to learn.
-       Learn Java, earn money?
-       Java is easy if you study well.
-       To learn Java, type codes.
-    3)Type code to print every word is used how many times in the text.
+       TODO:
+         1)Create a text file inside this package
+         2)Type the following text inside the text file
+         Java is easy to learn.
+         Learn Java, earn money?
+         Java is easy if you study well.
+         To learn Java, type codes.
+         3)Type code to print every word is used how many times in the text.
    */
 
     public static void main(String[] args) throws IOException {
+        // Dosya yolu
+        String filePath = "src/main/java/javaQuestions/interviewQuestions9/Q08_FileText.txt";
 
+        // HashMap oluşturma
+        HashMap<String, Integer> wordCounts = new HashMap<>();
 
+        try {
+            // Dosyayı okuma
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filePath)));
+
+            // Satır satır okuma
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                // Her satırı kelimelere ayırma
+                String[] words = line.split(" ");
+
+                // Her kelimenin tekrar sayısını hesaplama
+                for (String word : words) {
+                    if (wordCounts.containsKey(word)) {
+                        int count = wordCounts.get(word);
+                        wordCounts.put(word, count + 1);
+                    } else {
+                        wordCounts.put(word, 1);
+                    }
+                }
+            }
+
+            // HashMap'deki her kelime ve tekrar sayısını yazdırma
+            for (String word : wordCounts.keySet()) {
+                int count = wordCounts.get(word);
+                System.out.println(word + ": " + count);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Dosya okuma hatası: " + e.getMessage());
+        }
     }
 }
